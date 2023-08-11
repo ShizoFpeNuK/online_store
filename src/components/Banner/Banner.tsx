f23dc046e0a7396b82a41b8b186140cca1074045
Shiza <vnchernovv02@gmail.com>
2023-08-11 21:44:23 +0300

    add comp Baner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import styles from "./Banner.module.scss";
 import { FC } from "react";
 
 const Banner: FC = () => {
-	return <div>Banner</div>;
+	return (
+		<section className={styles.banner}>
+			<div className={styles.left}>
+				<p className={styles.content}>
+					NEW YEAR
+					<span>SALE</span>
+					<button className={styles.more}>See more</button>
+				</p>
+			</div>
+			<div className={styles.right}>
+				<Image
+					className={styles.right}
+					src="/banner.png"
+					alt="Sales"
+					width={637}
+					height={570}
+				/>
+				<p className={styles.discount}>
+					save up to <span>50%</span> off
+				</p>
+			</div>
+		</section>
+	);
 };
 
 export default Banner;
