16b68ade14b8f7544243194abe2be8fa98103859
Shiza <vnchernovv02@gmail.com>
2023-08-11 21:41:30 +0300

    some changes

--- next.config.js
+++ next.config.js
@@ -8,6 +8,42 @@ const nextConfig = {
 				port: "",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "fastly.picsum.photos",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "baratazo.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.redd.it",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "infobae.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "inhouse.infobae.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "dfcdn.defacto.com.tr",
+				port: "",
+				pathname: "/**",
+			},
 		],
 	},
 };
